85f506c313a71122fdbe145837b4e4b2e86556dd
vl-blinov <blinov.va93@gmail.com>
2022-03-28 19:07:18 +0300

    add project

---
+++ src/test/java/ru/blinov/control/inventory/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.blinov.control.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
