0d003ef4aa26ea2a1a3063ad3660bd6ee184d607
vl-blinov <blinov.va93@gmail.com>
2022-04-21 14:08:56 +0300

    delete fetchtype.lazy

--- src/main/java/ru/blinov/control/inventory/entity/InventoryCard.java
+++ src/main/java/ru/blinov/control/inventory/entity/InventoryCard.java
@@ -5,6 +5,7 @@ import java.time.ZonedDateTime;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,9 +37,9 @@ public class InventoryCard {
 	
 	@JsonIdentityInfo(generator=ObjectIdGenerators.UUIDGenerator.class, property="@id")
 	@ManyToOne(cascade= {CascadeType.DETACH,
-						 CascadeType.MERGE,
-						 CascadeType.PERSIST,
-						 CascadeType.REFRESH})
+					     CascadeType.MERGE,
+					     CascadeType.PERSIST,
+					     CascadeType.REFRESH})
 	@JoinColumn(name="user_id")
 	private User user;
 	
