d7fa970c254fb3c40392d2d6c5f2d084c8d5f994
vl-blinov <blinov.va93@gmail.com>
2022-10-06 18:23:39 +0300

    Update UserRepository

--- src/main/java/ru/blinov/control/inventory/repository/UserRepository.java
+++ src/main/java/ru/blinov/control/inventory/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package ru.blinov.control.inventory.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.blinov.control.inventory.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	public User findByUsername(String username);
+	public Optional<User> findByUsername(String username);
 }
