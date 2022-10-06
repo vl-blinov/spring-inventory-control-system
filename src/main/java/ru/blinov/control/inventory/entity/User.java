50c4fe30ba0994d41994385aa7d7a0b5a25a25a8
vl-blinov <blinov.va93@gmail.com>
2022-10-06 18:21:24 +0300

    Update User entity

--- src/main/java/ru/blinov/control/inventory/entity/User.java
+++ src/main/java/ru/blinov/control/inventory/entity/User.java
@@ -21,7 +21,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
-	private int id;
+	private Integer id;
 	
 	@Column(name="username")
 	private String username;
@@ -79,11 +79,11 @@ public class User {
 		this.email = email;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
